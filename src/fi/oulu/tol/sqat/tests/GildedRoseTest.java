e3fcb8ee8644326e8ca80a94d7d1be89bcd51a6f
alfitra.20 <alfitrarahman@gmail.com>
2017-09-21 23:33:09 +0300

    Assignment Finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -34,6 +34,7 @@ public class GildedRoseTest {
 		Item itemBrie = items.get(0);
 		assertEquals(11, itemBrie.getQuality());
 	}
+	//Aged Brie
 	@Test
 	public void testIfSellInAvailable() {
 		GildedRose store = new GildedRose();
@@ -47,7 +48,172 @@ public class GildedRoseTest {
 		Item itemBrie = items.get(0);
 		assertEquals(2, itemBrie.getSellIn());
 	}
-	
+		//Aged Brie - quality never negative
+		@Test
+		public void AgedBrieQualityNeverNegativeTest() {
+			GildedRose store = new GildedRose();
+			store.addItem(new Item("Aged Brie", 0, 0) );
+
+			// Act
+			store.updateEndOfDay();
+			
+			// Assert
+			List<Item> items = store.getItems();
+			Item itemBrie = items.get(0);
+			assertEquals(2, itemBrie.getQuality());
+		}
+		//Dexterity Vest - quality never negative
+		@Test
+		public void DexterityVestQualityNeverNegativeTest() {
+			GildedRose store = new GildedRose();
+			store.addItem(new Item("+5 Dexterity Vest", 0, 0));
+
+			// Act
+			store.updateEndOfDay();
+			
+			// Assert
+			List<Item> items = store.getItems();
+			Item itemBrie = items.get(0);
+			assertEquals(0, itemBrie.getQuality());
+		}
+		// Elixir - quality never negative
+		@Test
+		public void ElixirQualityNeverNegativeTest() {
+			GildedRose store = new GildedRose();
+			store.addItem(new Item("Elixir of the Mongoose", 0, 0));
+
+			// Act
+			store.updateEndOfDay();
+			
+			// Assert
+			List<Item> items = store.getItems();
+			Item itemBrie = items.get(0);
+			assertEquals(0, itemBrie.getQuality());
+		}
+		// Sulfuras - quality never negative
+		@Test
+		public void SulfurasQualityNeverNegativeTest() {
+			GildedRose store = new GildedRose();
+			store.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 0));
+
+			// Act
+			store.updateEndOfDay();
+			
+			// Assert
+			List<Item> items = store.getItems();
+			Item itemBrie = items.get(0);
+			assertEquals(0, itemBrie.getQuality());
+		}
+		// Backstage passes - quality never negative
+		@Test
+		public void BackstagePassesQualityNeverNegativeTest() {
+			GildedRose store = new GildedRose();
+			store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0));
+			// Act
+			store.updateEndOfDay();
+			
+			// Assert
+			List<Item> items = store.getItems();
+			Item itemBrie = items.get(0);
+			assertEquals(0, itemBrie.getQuality());
+		}
+		// Conjured - quality never negative
+		@Test
+		public void ConjuredQualityNeverNegativeTest() {
+			GildedRose store = new GildedRose();
+			store.addItem(new Item("Conjured Mana Cake", 0, 0));
+			// Act
+			store.updateEndOfDay();
+			
+			// Assert
+			List<Item> items = store.getItems();
+			Item itemBrie = items.get(0);
+			assertEquals(0, itemBrie.getQuality());
+		}
+		
+	//Quality never gets more than 50 except Sulfura
+		//Aged Brie - Quality never gets more than 50
+				@Test
+				public void AgedBrieQualityNeverMoreThan50Test() {
+					GildedRose store = new GildedRose();
+					store.addItem(new Item("Aged Brie", 1, 50) );
+
+					// Act
+					store.updateEndOfDay();
+					
+					// Assert
+					List<Item> items = store.getItems();
+					Item itemBrie = items.get(0);
+					assertEquals(50, itemBrie.getQuality());
+				}
+				//Dexterity Vest - Quality never gets more than 50
+				@Test
+				public void DexterityVestQualityNeverMoreThan50Test() {
+					GildedRose store = new GildedRose();
+					store.addItem(new Item("+5 Dexterity Vest", 1, 50));
+
+					// Act
+					store.updateEndOfDay();
+					
+					// Assert
+					List<Item> items = store.getItems();
+					Item itemBrie = items.get(0);
+					assertEquals(49, itemBrie.getQuality());
+				}
+				// Elixir - Quality never gets more than 50
+				@Test
+				public void ElixirQualityNeverMoreThan50Test() {
+					GildedRose store = new GildedRose();
+					store.addItem(new Item("Elixir of the Mongoose", 1, 50));
+
+					// Act
+					store.updateEndOfDay();
+					
+					// Assert
+					List<Item> items = store.getItems();
+					Item itemBrie = items.get(0);
+					assertEquals(49, itemBrie.getQuality());
+				}
+				// Sulfuras - Quality never gets more than 80
+				@Test
+				public void SulfurasQualityNeverMoreThan80Test() {
+					GildedRose store = new GildedRose();
+					store.addItem(new Item("Sulfuras, Hand of Ragnaros", 1, 80));
+
+					// Act
+					store.updateEndOfDay();
+					
+					// Assert
+					List<Item> items = store.getItems();
+					Item itemBrie = items.get(0);
+					assertEquals(80, itemBrie.getQuality());
+				}
+				// Backstage passes - Quality never gets more than 50
+				@Test
+				public void BackstagePassesQualityNeverMoreThan50Test() {
+					GildedRose store = new GildedRose();
+					store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 2, 50));
+					// Act
+					store.updateEndOfDay();
+					
+					// Assert
+					List<Item> items = store.getItems();
+					Item itemBrie = items.get(0);
+					assertEquals(50, itemBrie.getQuality());
+				}
+				// Conjured - Quality never gets more than 50
+				@Test
+				public void ConjuredQualityNeverMoreThan50Test() {
+					GildedRose store = new GildedRose();
+					store.addItem(new Item("Conjured Mana Cake", 2, 50));
+					// Act
+					store.updateEndOfDay();
+					
+					// Assert
+					List<Item> items = store.getItems();
+					Item itemBrie = items.get(0);
+					assertEquals(49, itemBrie.getQuality());
+				}
 	@Test
 	public void testIfAgedbrieHaveHigherQualityAsItGetsOlder() {
 		GildedRose store = new GildedRose();
@@ -65,43 +231,43 @@ public class GildedRoseTest {
 		assertEquals(15, itemBrie.getQuality());
 	}
 	
-	@Test
-	public void testIfAgedbrieQualityNeverPassed50() {
-		GildedRose store = new GildedRose();
-		store.addItem(new Item("Aged Brie", 3, 10) );
+	//@Test
+	//public void testIfAgedbrieQualityNeverPassed50() {
+	//	GildedRose store = new GildedRose();
+	//	store.addItem(new Item("Aged Brie", 3, 10) );
 		
 		// Act
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
-		store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
 		//already reach 50 
-		store.updateEndOfDay();
-		store.updateEndOfDay();
+	//	store.updateEndOfDay();
+	//	store.updateEndOfDay();
 		
 		// Assert
-		List<Item> items = store.getItems();
-		Item itemBrie = items.get(0);
-		assertEquals(50, itemBrie.getQuality());
+	//	List<Item> items = store.getItems();
+	//	Item itemBrie = items.get(0);
+	//	assertEquals(50, itemBrie.getQuality());
 		
-	}
+	//}
 	
 	@Test
 	public void testIfSulfuraNeverDecreaseQuality() {
